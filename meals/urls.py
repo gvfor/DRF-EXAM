6077671ef1154fc5579c06ff4f8598f40e654864
gvfor <gvfor7449@gmail.com>
2025-03-04 12:38:12 +0530

    intial commit

--- meals/urls.py
+++ meals/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('users/',views.UsercreateView.as_view()),
     path('meals/',views.MealListCreateView.as_view()),
     path('meals/<int:pk>/',views.MealRetriveUpdateDestroyView.as_view()),
+    path('meals/',views.GetmealsView.as_view()),
+    path('summary/',views.Mealsummaryview.as_view())
    
    
 ]
--- meals/views.py
+++ meals/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from meals.models import Meal
 from meals.serializers import UserSerializer,MealSerializer
 from rest_framework import authentication,permissions
+from rest_framework.views import APIView
+from django.db.models import Sum
+
 
 
 
@@ -33,3 +36,46 @@ class MealRetriveUpdateDestroyView(RetrieveAPIView,UpdateAPIView,DestroyAPIView)
     serializer_class=MealSerializer
 
     queryset=Meal.objects.all()
+
+class GetmealsView(APIView):
+     
+  def get(self,request):
+
+    date=request.GET.get('date')
+
+    meal_type=request.GET.get('meal_type')
+
+    meals=Meal.objects.all()
+
+    if date:
+
+        meals=meals.filter(date=date)
+    
+    if meal_type:
+
+        meals=meals.filter(meal_type=meal_type)
+
+    serializer=MealSerializer(meals, many=True)
+
+    return Response(serializer.data)
+
+
+class Mealsummaryview(APIView):
+
+    authentication_classes=[authentication.BasicAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get(self,request,*args,**kwargs):
+
+        total_calorie=Meal.objects.filter(owner=request.user).values("calorie").aggregate(total=Sum("calorie"))
+
+        print(total_calorie)
+
+        category_summary=Meal.objects.filter(owner=request.user).values("meal_type").annotate(total=Sum("meal_type"))
+
+        context={
+            "expense_total":total_calorie.get("total"),
+            "category_summary":category_summary
+        }
+
+        return Response(data=context)    
